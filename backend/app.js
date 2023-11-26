4a215804e49c3523dcaeae0630ce323f7e82a5ac
ShihabSarar12 <shihabsararchamok@gmail.com>
2023-11-26 11:46:07 +0600

    added update and deletion

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,18 @@ import cors from 'cors';
 import {
     getUsers,
     getUser,
-    registerUser
+    registerUser,
+    deleteUser,
+    updateUserInfo
 } from './database.js';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.get('/', ( req, res ) =>{
+    res.send('Server is Running!!');
+});
 
 app.get('/users',async ( req, res ) =>{
     const users = await getUsers();
@@ -20,19 +26,44 @@ app.post('/users',async ( req, res ) =>{
     const user = await registerUser(userName, userEmail, userPassword);
     if(user){
         res.status(201).send(user);
+        return;
     }
-    else{
-        res.status(423).send('User already exists!!');
-    }
-    
+    res.status(423).send('User already exists!!');
 });
 
 app.get('/user/:id',async ( req, res ) =>{
     const { id } = req.params;
     const user = await getUser(id);
-    res.status(200).send(user);
+    if(user){
+        res.status(200).send(user);
+        return;
+    }
+    res.status(423).send('User doesn\'t exist!!');
 });
+
+app.delete('/user/:id', async ( req, res ) =>{
+    const { id } = req.params;
+    const { success } = await deleteUser(id);
+    console.log(success);
+    if(success){
+        res.status(200).send('Deletion Successful');
+        return;
+    }
+    res.status(423).send('Deletion Failed');
+})
+
+app.patch('/user/:id', async ( req, res ) =>{
+    const { id } = req.params;
+    const { userName, userEmail, userPassword } = req.body;
+    const { success, user } = await updateUserInfo( id, userName, userEmail, userPassword );
+    if(success){
+        res.status(200).send(user);
+        return;
+    }
+    res.status(423).send('Update failed');
+})
 
 app.listen(process.env.SERVER_PORT, () =>{
     console.log('Server is running on ' + process.env.SERVER_PORT);
+    console.log(`Listening on http://localhost:${process.env.SERVER_PORT}/`);
 })
--- backend/database.js
+++ backend/database.js
@@ -11,7 +11,6 @@ const pool = mysql.createPool({
 
 export const registerUser = async (userName, userEmail, userPassword) =>{
     const [ userExist ] = await pool.query(`SELECT * FROM users WHERE userName = ?`, [userName]);
-    console.log(userExist);
     if(userExist.length !== 0){
         console.log('User already exists!!');
         return null;
@@ -28,6 +27,40 @@ export const getUsers = async () =>{
 
 export const getUser = async (userId) =>{
     const [ result, fields ] = await pool.query(`SELECT * FROM users WHERE userID = ?;`, [userId]);
+    if(result.length === 0){
+        return null;
+    }
     return result[0];
+}
+
+export const deleteUser = async (userId) =>{
+    const [ { affectedRows }, fields ] = await pool.query(`DELETE FROM users WHERE userID = ?;`, [userId]);
+    if(affectedRows > 0){
+        return {
+            success: true
+        };
+    }
+    return {
+        success: false
+    };
+}
+
+export const updateUserInfo = async (userId, userName, userEmail, userPassword) =>{
+    const [ { affectedRows }, fields ] = await pool.query(`UPDATE users SET userName = ?, userEmail = ?, userPassword = ? WHERE userID = ?`, [ userName, userEmail, userPassword, userId ]);
+    if(affectedRows > 0){
+        return {
+            success: true,
+            user: {
+                userId,
+                userName,
+                userEmail,
+                userPassword
+            }
+        };
+    }
+    return {
+        success: false,
+        user: undefined
+    }
 }
 
