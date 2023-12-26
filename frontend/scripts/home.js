12b1761ea710478f1a6e4fe95c9ceecc2a366e93
ShihabSarar12 <shihabsararchamok@gmail.com>
2023-12-26 15:40:31 +0600

    fixed carousel

--- frontend/scripts/home.js
+++ frontend/scripts/home.js
@@ -1,3 +1,39 @@
-document.getElementById('left_btn').addEventListener('click', ()=>{
-    
+const slides = document.querySelectorAll('.slides');
+let counter = 0;
+slides.forEach((slide, index) =>{
+    slide.style.left = `${100 * index}%`;
 });
+
+const slideImage = () =>{
+    slides.forEach((slide) =>{
+        slide.style.transform = `translateX(-${counter * 100}%)`;
+    });
+}
+
+setInterval(() =>{
+    if(counter >= 5){
+        counter = 0;
+        return;
+    }
+    counter++;
+    slideImage();
+},5000);
+
+document.getElementById('carousel_left').addEventListener('click', ()=>{
+    if(counter <= 0){
+        counter = 5;
+        return;
+    }
+    counter--;
+    slideImage();
+});
+
+document.getElementById('carousel_right').addEventListener('click', ()=>{
+    if(counter >= 5){
+        counter = 0;
+        return;
+    }
+    counter++;
+    slideImage();
+});
+
